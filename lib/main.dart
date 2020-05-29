8396f047905c45996988381c6390e58f4daa384b
depidsvy <spidgorny@gmail.com>
2020-05-29 22:58:38 +0200

    - add saving data to storage - add loading data from storage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:json_store/json_store.dart';
 
 import 'DayData.dart';
 import 'SimpleTimeSeriesChart.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Glucose Insulin Hunger',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -35,19 +36,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final fiveHours = Duration(hours: 3);
   DayData day;
 
   @override
   void initState() {
     super.initState();
-    DateTime today = Jiffy().startOf(Units.DAY);
+    this.loadFromStorage();
+//    this.sampleData();
+//    this.saveDay();
+    new Timer.periodic(Duration(seconds: 60), (Timer t) => setState(() {}));
+  }
+
+  loadFromStorage() async {
+    JsonStore jsonStore = JsonStore();
+//    DateTime today = this.today();
+    String ymd = this.ymd();
+//    print(['ymd', ymd]);
+    Map<String, dynamic> json = await jsonStore.getItem(ymd);
+//    print(['json', json]);
+    if (json != null) {
+      setState(() {
+        this.day = DayData.fromJson(json);
+      });
+    }
+  }
+
+  String ymd() {
+    String ymd = Jiffy(this.today()).format('y-MM-dd');
+    return ymd;
+  }
+
+  /// @deprecated
+  void sampleData() {
+    DateTime today = this.today();
 //    print(['today', today]);
     this.day = DayData(today, [
       new Ate("10:30", 0.7),
       new Ate("12:30", 1),
       new Ate("17:00", 1),
     ]);
-    new Timer.periodic(Duration(seconds: 60), (Timer t) => setState(() {}));
+  }
+
+  saveDay() async {
+    JsonStore jsonStore = JsonStore();
+//    DateTime today = this.today();
+    String ymd = this.ymd();
+    print(['ymd', this.day.toJson()]);
+    await jsonStore.setItem(ymd, this.day.toJson());
+  }
+
+  DateTime today() {
+    DateTime today = Jiffy().startOf(Units.DAY);
+    return today;
   }
 
   @override
@@ -62,29 +103,14 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Expanded(
                 child: SimpleTimeSeriesChart(
-              this.createChartData(),
+              this.createChartSeries(),
               // Disable animations for image tests.
               animate: true,
             )),
             Expanded(
-                child: ListView.builder(
-                    itemCount: day.intake.length,
-                    itemBuilder: (context, index) {
-                      final item = day.intake[index];
-                      var prev = index > 0 ? day.intake[index - 1] : null;
-
-                      return ListTile(
-                        title: Text(
-                          item.time,
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                        subtitle: Text('Meal size: ${item.amount}'),
-                        trailing: item.hoursSince(prev) != null
-                            ? Text(
-                                'Break: ${(item.hoursSince(prev).inMinutes / 60).toStringAsFixed(2)}h')
-                            : null,
-                      );
-                    }))
+                child: this.day != null
+                    ? renderMealList()
+                    : Center(child: CircularProgressIndicator()))
           ],
         ),
       ),
@@ -96,8 +122,32 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  List<charts.Series<TimeSeriesSales, DateTime>> createChartData() {
+  ListView renderMealList() {
+    return ListView.builder(
+        itemCount: day.intake.length,
+        itemBuilder: (context, index) {
+          final item = day.intake[index];
+          var prev = index > 0 ? day.intake[index - 1] : null;
+
+          return ListTile(
+            title: Text(
+              item.time,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            subtitle: Text('Meal size: ${item.amount}'),
+            trailing: item.hoursSince(prev) != null
+                ? Text(
+                    'Break: ${(item.hoursSince(prev).inMinutes / 60).toStringAsFixed(2)}h')
+                : null,
+          );
+        });
+  }
+
+  List<TimeSeriesSales> createChartData() {
     List<TimeSeriesSales> data = [];
+    if (this.day == null) {
+      return data;
+    }
     var today = Jiffy(this.day.date).startOf(Units.DAY);
     data.add(new TimeSeriesSales(today, 0)); // start day sleeping and hungry
     var fiveMinutes = Duration(minutes: 5);
@@ -115,16 +165,15 @@ class _MyHomePageState extends State<MyHomePage> {
         print('  not last');
         var next = this.day.intake[i + 1];
         var timeTillNext = next.dateTime.difference(ate.dateTime);
-        var fiveHours = Duration(hours: 5);
-        if (timeTillNext.compareTo(fiveHours) < 0) {
+        if (timeTillNext.compareTo(this.fiveHours) < 0) {
           print(['  timeTillNext is < than 5h', timeTillNext.toString()]);
-          var partialTime = timeTillNext.inSeconds / fiveHours.inSeconds;
+          var partialTime = timeTillNext.inSeconds / this.fiveHours.inSeconds;
           var height = (ate.amount * partialTime * 100).round();
           print(['  partialTime', partialTime, height]);
           data.add(new TimeSeriesSales(
               Jiffy(next.dateTime).subtract(duration: fiveMinutes), height));
         } else {
-          var ateTimePlus5 = ateTime.add(fiveHours);
+          var ateTimePlus5 = ateTime.add(this.fiveHours);
           print([
             '  next food in > than 5h, add "0" at',
             ateTimePlus5.hour.toString() + ':' + ateTimePlus5.minute.toString()
@@ -149,7 +198,10 @@ class _MyHomePageState extends State<MyHomePage> {
         .map((el) => Jiffy(el.time).Hm + ' [' + el.sales.toString() + ']')
         .toList());
     data = smoothSeries(data);
+    return data;
+  }
 
+  List<charts.Series<TimeSeriesSales, DateTime>> createChartSeries() {
     /////
     final myTabletData = [
       new TimeSeriesSales(Jiffy().dateTime, -50),
@@ -162,7 +214,7 @@ class _MyHomePageState extends State<MyHomePage> {
         colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
         domainFn: (TimeSeriesSales sales, _) => sales.time,
         measureFn: (TimeSeriesSales sales, _) => sales.sales,
-        data: data,
+        data: this.createChartData(),
       ),
       new charts.Series<TimeSeriesSales, DateTime>(
         id: 'Tablet',
